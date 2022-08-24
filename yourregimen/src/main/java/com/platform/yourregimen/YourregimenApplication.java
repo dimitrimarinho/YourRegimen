cb782867126a591121123d5040cc6d6de0417bd8
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2022-08-24 09:59:18 -0300

    Start Project with Sprint Initializr

---
+++ yourregimen/src/main/java/com/platform/yourregimen/YourregimenApplication.java
@@ -0,0 +1,13 @@
+package com.platform.yourregimen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourregimenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourregimenApplication.class, args);
+	}
+
+}
---
+++ yourregimen/src/test/java/com/platform/yourregimen/YourregimenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platform.yourregimen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourregimenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
