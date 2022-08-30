bb058ae6af8590d0a632ab524f3886f542e1e89e
isabela <isabelapiana91@gmail.com>
2022-08-30 14:54:03 -0300

    implementando camada de segurança

--- yourregimen/src/main/java/com/platform/yourregimen/admin/repository/AdminRepository.java
+++ yourregimen/src/main/java/com/platform/yourregimen/admin/repository/AdminRepository.java
@@ -1,10 +1,12 @@
-package com.platform.yourregimen.admin;
+package com.platform.yourregimen.admin.repository;
 
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.platform.yourregimen.admin.model.Admin;
 
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, Long>{
