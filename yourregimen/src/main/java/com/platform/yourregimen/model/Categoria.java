a21ad9de2c85d2f0063ddd8b5c1c46c905a322c2
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2022-08-31 17:34:20 -0300

    Adjustments made for testing

--- yourregimen/src/main/java/com/platform/yourregimen/model/Categoria.java
+++ yourregimen/src/main/java/com/platform/yourregimen/model/Categoria.java
@@ -1,18 +1,13 @@
 package com.platform.yourregimen.model;
 
-import java.util.List;
 import java.util.UUID;
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Type;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_categoria")
