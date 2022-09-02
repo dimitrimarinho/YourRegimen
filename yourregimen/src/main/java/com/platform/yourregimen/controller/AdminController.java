2038803ad361bee304244e741994b1a09308b491
isabela <isabelapiana91@gmail.com>
2022-09-02 13:46:37 -0300

    add delete - crud admin

--- yourregimen/src/main/java/com/platform/yourregimen/controller/AdminController.java
+++ yourregimen/src/main/java/com/platform/yourregimen/controller/AdminController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -64,6 +65,11 @@ public class AdminController {
 		return service.atualizarUsuario(usuario)
 			.map(resp -> ResponseEntity.status(HttpStatus.OK).body(resp))
 			.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete (@PathVariable UUID id) {
+		adminRepository.deleteById(id);
 	}
 	
 }
