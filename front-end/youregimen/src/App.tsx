9ca711958783508d005811c2b873df78a5932885
machado oliveira <stefanymoliveira87@gmail.com>
2022-09-15 09:45:34 -0300

    creating login component

--- front-end/youregimen/src/App.tsx
+++ front-end/youregimen/src/App.tsx
@@ -2,14 +2,27 @@ import React from 'react';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import './App.css';
+import Login from './paginas/Login/Login';
 
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      <Router>
+      
+   <div style={{ minHeight: '100vh' }}>
+   <Routes>
+   <Route path="/" element={<Login />} />
+   <Route path="/login" element={<Login />} />
+   <Route path="/home" element={<Home />} />
+   </Routes>
+   </div>
+
+   </Router>
+      
       <Footer />
     </>
   );
---
+++ front-end/youregimen/src/paginas/Login/Login.tsx
@@ -0,0 +1,49 @@
+import { Box, Button, Grid, TextField, Typography, Paper } from "@material-ui/core";
+import {Link} from "react-router-dom";
+
+import React from "react";
+
+
+import "./Login.css";
+
+function Login(){
+    return(
+        <Grid container direction="row" justifyContent="center" alignItems="center">
+        <Grid alignItems="center" xs={12} >
+            <Box width='100%' height='100vh' display={'flex'} alignItems='center' paddingLeft={'10%'} className='imagem' >
+                <Paper style={{padding:'32px 48px', borderRadius:'16px'}} className="login-c">
+                <Box maxWidth={'420px'}>
+                <form >
+                    <Typography variant="h3" gutterBottom  align="center" component="h3" className="texto">
+                        Bem vindo!
+                    </Typography>
+                    <TextField id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth required/>
+                        <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password'fullWidth required/>
+                    <Box marginTop={2} textAlign="center">
+                       <Link to="/home"></Link>
+                            <Button type='submit' className="botao">
+                                Logar
+                            </Button>
+                    </Box>
+                </form >
+                <Box display="flex" justifyContent="center" marginTop={2}>
+                    <Box marginRight={1}>
+                        <Typography variant="subtitle1" gutterBottom align="center">
+                            Não tem uma conta?
+                        </Typography>
+                    </Box>
+                    <Link to='/cadastrousuario' style={{textDecoration: 'none', color:'#40352C'}}>
+                    <Typography variant="subtitle1" gutterBottom align="center"  className="cursor">
+                        Cadastre-se
+                    </Typography>
+                    </Link>
+                </Box>
+            </Box>
+            </Paper>
+            </Box>
+        </Grid>
+    </Grid>
+    );
+}
+
+export default Login;
