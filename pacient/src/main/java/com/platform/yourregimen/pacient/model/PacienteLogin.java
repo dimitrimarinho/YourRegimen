434b5e814818e0b8c2b117ce9c3ca593a3b9ca0c
isabela <isabelapiana91@gmail.com>
2022-09-20 08:23:54 -0300

    fix model PacienteLogin

--- pacient/src/main/java/com/platform/yourregimen/pacient/model/PacienteLogin.java
+++ pacient/src/main/java/com/platform/yourregimen/pacient/model/PacienteLogin.java
@@ -3,6 +3,8 @@ package com.platform.yourregimen.pacient.model;
 import java.util.UUID;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Type;
 
@@ -21,13 +23,26 @@ public class PacienteLogin {
 	private String senha;
 	
 	private String token;
+	
+	private double altura;
 
-	public PacienteLogin(UUID id, String nome, String usuario, String senha, String token) {
+	private double peso;
+
+	private double maxCalories;
+
+	private double minCalories;
+
+	public PacienteLogin(UUID id, String nome, String usuario, String senha, String token, double altura, double peso,
+			double maxCalories, double minCalories) {
 		this.id = id;
 		this.nome = nome;
 		this.usuario = usuario;
 		this.senha = senha;
 		this.token = token;
+		this.altura = altura;
+		this.peso = peso;
+		this.maxCalories = maxCalories;
+		this.minCalories = minCalories;
 	}
 
 	public PacienteLogin() {
@@ -71,6 +86,38 @@ public class PacienteLogin {
 
 	public void setToken(String token) {
 		this.token = token;
+	}
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+
+	public double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+
+	public double getMaxCalories() {
+		return maxCalories;
+	}
+
+	public void setMaxCalories(double maxCalories) {
+		this.maxCalories = maxCalories;
+	}
+
+	public double getMinCalories() {
+		return minCalories;
+	}
+
+	public void setMinCalories(double minCalories) {
+		this.minCalories = minCalories;
 	}
 		
 }
