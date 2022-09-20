33cca49d605db53f2212f0e8aaa10b7a3c4b821c
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2022-09-20 10:53:11 -0300

    Microservice Pacient - Heroku Deploy

--- src/main/java/com/platform/yourregimen/pacient/security/BasicSecurityConfig.java
+++ src/main/java/com/platform/yourregimen/pacient/security/BasicSecurityConfig.java
@@ -35,13 +35,16 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
 		http.authorizeRequests()
-		.antMatchers("/pacientes/logar").permitAll()
-		.antMatchers("/pacientes/cadastrar").permitAll()
-		.antMatchers(HttpMethod.OPTIONS).permitAll()
-		.anyRequest().authenticated()
-		.and().httpBasic()
-		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		.and().cors()
-		.and().csrf().disable();
+			.antMatchers("/pacientes/logar").permitAll()
+			.antMatchers("/pacientes/cadastrar").permitAll()
+			.antMatchers(HttpMethod.OPTIONS).permitAll()
+			.anyRequest().authenticated()
+			.and().httpBasic()
+			.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and().cors()
+			.and().csrf().disable();
+		security
+        	.httpBasic().disable()
+            .formLogin().disable();
 	}
 }
