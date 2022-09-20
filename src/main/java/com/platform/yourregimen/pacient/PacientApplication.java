035574d529f87712807fab7f5c719216fefc0b71
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2022-09-20 10:08:24 -0300

    Microservice Pacient - Heroku Deploy

--- src/main/java/com/platform/yourregimen/pacient/PacientApplication.java
+++ src/main/java/com/platform/yourregimen/pacient/PacientApplication.java
@@ -3,8 +3,8 @@ package com.platform.yourregimen.pacient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-//@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+//@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class PacientApplication {
 
 	public static void main(String[] args) {
