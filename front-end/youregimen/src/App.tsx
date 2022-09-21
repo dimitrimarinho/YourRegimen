97f0bdf5e6dc29bb956a630df40f3fe5e0f0bf0a
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2022-09-21 14:54:30 -0300

    Adjustments

--- front-end/youregimen/src/App.tsx
+++ front-end/youregimen/src/App.tsx
@@ -14,7 +14,7 @@ import CadastroRegimen from './components/regimens/cadastroRegimen/CadastroRegim
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 import store from './store/store';
-
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
