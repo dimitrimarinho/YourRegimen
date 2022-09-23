99afd2dbccce2adb92fbebf8cf01895a4520ec3f
machado oliveira <stefanymoliveira87@gmail.com>
2022-09-23 08:49:55 -0300

    implement: logout

--- src/service/Service.ts
+++ src/service/Service.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const api = axios.create({
     baseURL: 'https://cors-anywhere.herokuapp.com/https://api-gateway--microservice.herokuapp.com'
